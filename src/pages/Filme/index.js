dcb15d2e5552d406ee430f271dbf20af94bc671a
Danilo Santilli <danilo-santilli@hotmail.com>
2023-05-08 15:31:43 +0100

    Encaminhando para o trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,48 +1,43 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import './filme-info.css';
 
 import api from '../../Services/api';
 
 function Filme(){
 
-    // Obtém o parâmetro de rota contendo o ID do filme
     const { id } = useParams(); 
+    const navigate = useNavigate();
 
-    // Define o estado do filme e de loading
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         async function loadFilme(){
-            // Faz uma requisição à API do TMDB buscando o filme pelo ID
             await api.get(`/movie/${id}`, {
                 params:{
                     api_key: '689d6c2e5f0a5e5187dc23b26f775e43',
                 }
             })
             .then((response)=>{
-                // Define o estado do filme com os dados retornados pela API
                 setFilme(response.data);
-                // Define o estado de loading como false
                 setLoading(false);
             })
             .catch(()=>{
-                console.log('Filme não encontrado.')
+                console.log('Filme não encontrado.');
+                navigate('/', {replace:true});
+                return;
             })
         }
 
-        // Chama a função para carregar o filme ao montar o componente
         loadFilme();
 
-        // Função de limpeza que é chamada ao desmontar o componente
         return()=>{
             console.log('Componente foi desmontado');
         }
-    }, []);
+    }, [navigate, id]);
 
     if (loading) {
-        // Se ainda estiver carregando, exibe uma mensagem de loading
         return(
             <div className="filme-info">
                 <h1>Carregando Filme...</h1>
@@ -50,7 +45,6 @@ function Filme(){
         );
     }
 
-    // Quando o filme estiver carregado, exibe suas informações
     return(
         <div className="filme-info">
             <h1>{filme.title}</h1>
@@ -65,7 +59,7 @@ function Filme(){
             <div className="area-buttons">
                 <button>Salvar</button>
                 <button>
-                    <a href="#">Trailer</a>
+                    <a target="_blank" rel="external noreferrer" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
                 </button>
             </div>
         </div>
