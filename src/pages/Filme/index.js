01ce03f8121a8d0c2178b7d1faf62ea9f05ad07d
Danilo Santilli <danilo-santilli@hotmail.com>
2023-05-08 13:56:04 +0100

    Detalhe do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,73 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './filme-info.css';
+
+import api from '../../Services/api';
+
 function Filme(){
+
+    // Obtém o parâmetro de rota contendo o ID do filme
+    const { id } = useParams(); 
+
+    // Define o estado do filme e de loading
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            // Faz uma requisição à API do TMDB buscando o filme pelo ID
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: '689d6c2e5f0a5e5187dc23b26f775e43',
+                }
+            })
+            .then((response)=>{
+                // Define o estado do filme com os dados retornados pela API
+                setFilme(response.data);
+                // Define o estado de loading como false
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log('Filme não encontrado.')
+            })
+        }
+
+        // Chama a função para carregar o filme ao montar o componente
+        loadFilme();
+
+        // Função de limpeza que é chamada ao desmontar o componente
+        return()=>{
+            console.log('Componente foi desmontado');
+        }
+    }, []);
+
+    if (loading) {
+        // Se ainda estiver carregando, exibe uma mensagem de loading
+        return(
+            <div className="filme-info">
+                <h1>Carregando Filme...</h1>
+            </div>
+        );
+    }
+
+    // Quando o filme estiver carregado, exibe suas informações
     return(
-        <div>
-            <h1>Filme</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img
+                src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}                
+                alt={filme.title}
+            />
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">Trailer</a>
+                </button>
+            </div>
         </div>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 import './home.css';
 
 function Home() {
-  // Define o estado dos filmes e inicializa como um array vazio.
   const [filmes, setFilmes] = useState([]);
-  // Define o estado de loading e inicializa como true.
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
-      // Faz a requisição dos filmes em cartaz usando a api criada anteriormente.
       const response = await api.get('movie/now_playing', {
         params: {
           api_key: '689d6c2e5f0a5e5187dc23b26f775e43',
@@ -19,16 +16,13 @@ function Home() {
         },
       });
 
-      // Atualiza o estado de filmes com os dados obtidos na requisição e filtra os 10 primeiros resultados.
       setFilmes(response.data.results.slice(0, 10));
-      // Muda o estado de loading para false para indicar que a requisição terminou de ser carregada.
       setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
-  // Verifica se ainda está carregando os filmes, caso sim, exibe uma mensagem.
   if (loading) {
     return(
         <div className='loading'>
@@ -37,7 +31,6 @@ function Home() {
     );
   }
 
-  // Caso contrário, renderiza a lista de filmes.
   return (
     <div className='container'>
       <div className='lista-filmes'>
